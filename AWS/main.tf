af6112e8e7fba6e2cddba64d762e5cab8283363f
Mariano Mirabelli <mariano.mirabelli@almundo.com>
2020-02-10 12:47:04 -0300

    refactor: folder structure

--- AWS/main.tf
+++ AWS/main.tf
@@ -5,13 +5,13 @@ provider "aws" {
 
 # Storage Resources
 module "storage" {
-  source       = "./storage"
+  source       = "./terraform-aws-storage"
   project_name = "${var.project_name}"
 }
 
 # Networking Resources
 module "networking"{
-  source = "./networking"
+  source = "./terraform-aws-networking"
   vpc_cidr = "${var.vpc_cidr}"
   public_cidrs = "${var.public_cidrs}"
   accessip = "${var.accessip}"
